ecc7b2eeb88702aed8ad30a5548cd2cf28de706e
Sara Fernández Arias <uo269546@uniovi.es>
2022-02-10 20:34:19 +0100

    SDI-2122-307-1.8-MVC, Modelo Vista Controlador.

--- src/main/java/com/uniovi/sdi/ServletShoppingCart.java
+++ src/main/java/com/uniovi/sdi/ServletShoppingCart.java
@@ -12,20 +12,35 @@ import java.util.HashMap;
 import java.util.Map;
 
 @WebServlet(name = "ServletShoppingCart", value = "/AddToShoppingCart")
+/**
+ * Este servlet hará el papel de controlador una vez recibida la petición.
+ *  -Comprueba si hay un carrito en sesión-> comprueba si la petición contiene un producto.
+ *  -Introduce el objeto cart como atributo a la petición con clave "selected items"
+ *  -Redirige la petición a la vista cart.jsp
+ */
 public class ServletShoppingCart extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         HttpSession session = request.getSession();
         HashMap<String, Integer> cart = (HashMap<String, Integer>) request.getSession().getAttribute("cart");
-// No hay carrito, creamos uno y lo insertamos en sesión
+        // No hay carrito, creamos uno y lo insertamos en sesión
         if (cart == null) {
             cart = new HashMap<String, Integer>();
             request.getSession().setAttribute("cart", cart);
         }
         String product = request.getParameter("product");
         if (product != null) {
+            //añadimos el producto seleccionado al carrito.
             addToShoppingCart(cart, product);
         }
+        //MVC:Retornamos la vista con parámetro "selecteditems"
+        request.setAttribute("selectedItems", cart);
+        getServletContext().getRequestDispatcher("/cart.jsp").forward(request, response);
+        //MVC:Retornamos la vista con parámetro "selecteditems"-fin
+
+
+        /**
+         * Código previo al modelo vista controlador.
         response.setCharacterEncoding("UTF-8");
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
@@ -33,9 +48,10 @@ public class ServletShoppingCart extends HttpServlet {
         out.println("<HEAD><TITLE>Tienda SDI: Cesta de la compra</TITLE></HEAD>");
         out.println("<BODY>");
 
-     //   out.println("<a href=\"OLD-shop.html\">Volver</a></BODY></HTML>");
+        //  out.println("<a href=\"OLD-shop.html\">Volver</a></BODY></HTML>");
         out.println(shoppingCartToHtml(cart) + "<br>");
         out.println("<a href=\"index.jsp\">Volver</a></BODY></HTML>");
+         */
     }
 
     private void addToShoppingCart(Map<String, Integer> cart, String productKey) {
